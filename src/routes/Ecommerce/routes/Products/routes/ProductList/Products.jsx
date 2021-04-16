9bfb4da7b57ea08df12738dd48d3d3536ab2249b
Max Moeschinger <m.moeschinger@gmail.com>
2021-04-16 14:46:20 +0200

    remove unused

--- src/routes/Ecommerce/routes/Products/routes/ProductList/Products.jsx
+++ src/routes/Ecommerce/routes/Products/routes/ProductList/Products.jsx
@@ -31,16 +31,6 @@ const Products = ({
     featuredTitle,
     history,
 }) => {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
     return (
         <Container>
             <Row>
