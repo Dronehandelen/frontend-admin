2d19360b5daf075b5f9f2040fa1c43e5d307d4c3
Max Moeschinger <max@momentech.no>
2021-07-06 18:46:26 +0200

    increase timeout

--- src/containers/BarcodeScanner.jsx
+++ src/containers/BarcodeScanner.jsx
@@ -30,7 +30,7 @@ const BarcodeScanner = ({ children }) => {
                     processEnd();
                     code = '';
                     reading = false;
-                }, 200);
+                }, 400);
             }
         });
     }, []);
